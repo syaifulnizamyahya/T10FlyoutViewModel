c3be7ac1d327f13f14c67dd011912c4e50344b3e
Syaiful Nizam Yahya <syaifulnizamyahya@gmail.com>
2017-04-27 11:43:47 +0800

    Add project files.

---
+++ T10FlyoutViewModel/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using Template10.Services.NavigationService;
+using Windows.UI.Xaml.Navigation;
+
+namespace T10FlyoutViewModel.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+
+        bool _IsFlyoutOpen = default(bool);
+        public bool IsFlyoutOpen { get { return _IsFlyoutOpen; } set { Set(ref _IsFlyoutOpen, value); } }
+
+
+        DelegateCommand _CloseFlyout;
+        public DelegateCommand CloseFlyout
+           => _CloseFlyout ?? (_CloseFlyout = new DelegateCommand(() =>
+           {
+               _IsFlyoutOpen = false;
+           }, () => true));
+
+
+        DelegateCommand _OpenFlyout;
+        public DelegateCommand OpenFlyout
+           => _OpenFlyout ?? (_OpenFlyout = new DelegateCommand(() =>
+           {
+               _IsFlyoutOpen = true;
+           }, () => true));
+    }
+}
